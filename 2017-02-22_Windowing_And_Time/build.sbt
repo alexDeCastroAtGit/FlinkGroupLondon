90e3a985423ad07e192a0107b16bdbdbf89d4f8d
Satyasheel <satyasheel@ymail.com>
2017-02-16 09:56:48 +0000

    commit

--- 2017-02-22_Windowing_And_Time/build.sbt
+++ 2017-02-22_Windowing_And_Time/build.sbt
@@ -3,7 +3,7 @@ name := "flink-Window-and-Time-Example"
 version := "1.0"
 scalaVersion := "2.11.7"
 
-libraryDependencies ++= Seq("org.apache.flink" %% "flink-scala" % "1.0.0",
-  "org.apache.flink" %% "flink-clients" % "1.0.0",
-  "org.apache.flink" %% "flink-streaming-scala" % "1.0.0")
+libraryDependencies ++= Seq("org.apache.flink" %% "flink-scala" % "1.2.0",
+  "org.apache.flink" %% "flink-clients" % "1.2.0",
+  "org.apache.flink" %% "flink-streaming-scala" % "1.2.0")
 
--- 2017-02-22_Windowing_And_Time/src/main/scala/co/uk/DRUK/flink/eventTime/EventTimeExample.scala
+++ 2017-02-22_Windowing_And_Time/src/main/scala/co/uk/DRUK/flink/eventTime/EventTimeExample.scala
@@ -9,27 +9,36 @@ import org.apache.flink.streaming.api.windowing.time.Time
 
 
 object EventTimeExample {
-
+  // model which tracks stock price in a given point of time.
   case class Stock(time:Long, symbol:String,value:Double)
 
   def main(args: Array[String]) {
-
+    // Obtain an execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    // setting EventTime as time characteristic for the environment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    // Listening socket for text stream
     val source = env.socketTextStream("localhost",50050)
+
+    // convert string network stream into Stock model --> "Stock(time:Long, symbol:String,value:Double)"
     val parsedStream = source.map(value => {
       val columns = value.split(",")
       Stock(columns(0).toLong, columns(1),columns(2).toDouble)
     })
 
+    // **ascending watermarks** --> confirms that at timestamp "t" no events with timestamp lower than "t" occurs
     val timedValue = parsedStream.assignAscendingTimestamps(_.time)
 
+    // Grouping the stream based on stock symbole
     val keyedStream = timedValue.keyBy(_.symbol)
 
+    // max stock value in a 10 sec interval.
     val timeWindow = keyedStream.timeWindow(Time.seconds(10)).max("value").name("timedwindow")
 
-    timeWindow.print.name("print sink")
+    // stdout
+    timeWindow.print.name("to sink")
     env.execute()
   }
 }
